64c8d760d68e785534f83a8fe48268de8a41d29e
hyperhkm <lucasjamaico@gmail.com>
2023-07-29 22:11:59 -0300

    .

---
+++ script.js
@@ -0,0 +1,8 @@
+const squares = document.querySelectorAll('.container>div'),
+      gameBoard = document.querySelector('.container'),
+      circle = document.querySelectorAll('.circle'),
+      plus = document.querySelectorAll('.plus');
+
+squares.forEach(square => {
+    
+})
